56830fd45a6900f3d451bb15aee9aec15a68cdf7
dznice <dizonnicer@gmail.com>
2023-03-27 14:15:40 +0800

    Add frontend components
    Included with the sidebar nav, dashboard, and members tab

---
+++ FRONTEND/src/app/sadmin-chpass/sadmin-chpass.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sadmin-chpass',
+  templateUrl: './sadmin-chpass.component.html',
+  styleUrls: ['./sadmin-chpass.component.scss']
+})
+export class SadminChpassComponent {
+
+}
