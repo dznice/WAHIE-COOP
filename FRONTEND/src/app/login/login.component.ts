55f65aa12060451e79f2fa2ce2391a581a5797f8
Alipioallen <111748932+Alipioallen@users.noreply.github.com>
2023-03-29 09:41:32 +0800

    Add Login and animations

--- FRONTEND/src/app/login/login.component.ts
+++ FRONTEND/src/app/login/login.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, OnDestroy, ViewEncapsulation, Inject  } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { formState, formState2, formState3, formState4, formState5, slideleft, slideright } from '../animation';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [formState, formState2, formState3, formState4, formState5, slideleft, slideright],
+  encapsulation: ViewEncapsulation.None,
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy  {
+
+  isDisplayed: boolean = true;
+  toggleDiv(){
+    this.isDisplayed = this.isDisplayed? false:true;
+  }
+  
+  visible:boolean = true;
+  changetype:boolean = true;
+  viewpass(){
+    this.visible = !this.visible
+    this.changetype = !this.changetype
+  }
+
+  cvisible:boolean = true;
+  cchangetype:boolean = true;
+  cviewpass(){
+    this.cvisible = !this.cvisible
+    this.cchangetype = !this.cchangetype
+  }
+
+  constructor(@Inject(DOCUMENT) private _document: any){  
+}
+
+  ngOnInit() {
+    this._document.body.classList.add('body');
+  }
+
+  ngOnDestroy() {
+    this._document.body.classList.add('body');
+  }
 
 }
