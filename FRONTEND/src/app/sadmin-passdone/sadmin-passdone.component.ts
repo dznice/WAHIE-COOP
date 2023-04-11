6eb4f198c1f9b9969c710dda3498ba810fd6dbd0
Alipioallen <111748932+Alipioallen@users.noreply.github.com>
2023-04-11 16:03:30 +0800

    Added Super-Admin chpass and passdone

--- FRONTEND/src/app/sadmin-passdone/sadmin-passdone.component.ts
+++ FRONTEND/src/app/sadmin-passdone/sadmin-passdone.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { slider, slideright} from '../animation';
 
 @Component({
   selector: 'app-sadmin-passdone',
   templateUrl: './sadmin-passdone.component.html',
-  styleUrls: ['./sadmin-passdone.component.scss']
+  styleUrls: ['./sadmin-passdone.component.scss'],
+  animations:[slideright, slider],
+  encapsulation: ViewEncapsulation.None,
 })
-export class SadminPassdoneComponent {
+export class SadminPassdoneComponent implements OnInit, OnDestroy {
+
+  isDisplayed: boolean = true;
+  toggleDiv(){
+    this.isDisplayed = this.isDisplayed? false:true;
+  }
+  
+  constructor(@Inject(DOCUMENT) private _document: any){}
+
+  ngOnInit() {
+    this._document.body.classList.add('body');
+  }
+
+  ngOnDestroy() {
+    this._document.body.classList.add('body');
+  }
 
 }
