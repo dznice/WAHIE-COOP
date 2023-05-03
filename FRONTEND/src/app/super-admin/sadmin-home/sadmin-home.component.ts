8087d60451439d8bd8bd641f9a1dd0f68b5fae24
Alipioallen <111748932+Alipioallen@users.noreply.github.com>
2023-05-03 16:51:51 +0800

    Super Admin activate disable button

--- FRONTEND/src/app/super-admin/sadmin-home/sadmin-home.component.ts
+++ FRONTEND/src/app/super-admin/sadmin-home/sadmin-home.component.ts
@@ -9,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class SadminHomeComponent {
 
   /* Switch declaration */
-  selected: boolean = false;
+  selected: boolean;
+
 
   constructor(private http: HttpClient){
     this.showUsers();
@@ -41,7 +42,10 @@ export class SadminHomeComponent {
       }
     )
   }
-  isChecked: boolean = true;
+  isChecked: boolean=true ;
+  isChecked2: boolean=false ;
+
+       
 
   getValue() {
     this.isChecked = !this.isChecked;
@@ -54,11 +58,23 @@ export class SadminHomeComponent {
     this.email = data.email
     this.coop = data.coop
     this.activated();
+    this.isChecked=false
+  }
+  deactivateTOactivate(data:any) {
+    this.id = data.id,
+    this.status = 0,
+    this.name = data.name,
+    this.email = data.email
+    this.deactivated();
+    this.isChecked2=false;
+
+    
   }
 
   activated(){
+  
     let updateStatus = {
-     'status': 1,
+     'status': 0,   
      'id' : this.id,
      'name': this.name,
      'email': this.email,
@@ -73,6 +89,29 @@ export class SadminHomeComponent {
         this.name = '';
         this.email = '';
         this.coop = '';
+        this.isChecked=false;
+      }
+    )
+    }
+
+  deactivated(){
+    let updateStatus2 = {
+      'status': 1, 
+      'id' : this.id,
+      'name': this.name,
+      'email': this.email,
+      
+    
+     }
+     this.http.put('http://127.0.0.1:8000/api/users' + '/' + this.id, updateStatus2).subscribe(
+      (res:any)=>{
+        console.log(res);
+        this.showUsers();
+        this.id = '';
+        this.status = 1;
+        this.name = '';
+        this.email = '';
+        this.isChecked=true;
       }
     )
   }
