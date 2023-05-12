2f03c241ab30692a8f6943321e305555c8bed414
CharlesManiaul <111725149+CharlesManiaul@users.noreply.github.com>
2023-05-12 13:19:28 +0800

    Admin Home items to show

--- FRONTEND/src/app/admin/admin-home/admin-home.component.ts
+++ FRONTEND/src/app/admin/admin-home/admin-home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from './hItem';
 import { itemService } from './hItem.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
@@ -8,7 +11,7 @@ import { itemService } from './hItem.service';
   providers: [itemService]
 })
 export class AdminHomeComponent implements OnInit {
-  item: Item[] = [];
+  item: any[] = [];
   term: string = '';
   p: number = 1;
   type: string;
@@ -22,10 +25,25 @@ export class AdminHomeComponent implements OnInit {
   startDate: string = '';
   endDate : string = '';
 
-  constructor(private ItemService: itemService) {}
+  constructor(private ItemService: itemService,
+              private http: HttpClient,
+              private route: Router) {}
   ngOnInit(): void {
     this.item = this.ItemService.item;
+      // this.showEntries();
+      this.showAccounting();
   }
+
   
+  public entries: any;
+  public account: any;
+  
+
+  showAccounting(): void {
+      this.http.get('http://127.0.0.1:8000/api/account').subscribe((res: any) => {
+        console.log(res);
+        this.account = res;
+      });
+    }
 
 }
--- FRONTEND/src/app/admin/admin-home/hItem.ts
+++ FRONTEND/src/app/admin/admin-home/hItem.ts
@@ -6,4 +6,26 @@ export class Item{
     DueDate: any;
     Balance: any;
     Status: any;
+debit: any;
+
+    account_id: number;
+    first_name : string;
+    email : string;
+    balance: string;
+
+    transaction_number: any;
+    transaction_date: Date;
+    id: any;
+    entry_name: any;
+    entry_id: any;
+    entries: any;
+    transac: any;
+    memberss_id: any;
+    open_balance: any;
+    cred: any;
+    status: any;
+    acc: any;
+    orig_amount: any;
+    paystats: any;
+
 }
