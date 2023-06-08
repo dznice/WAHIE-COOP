0d45460c60144317e7893e8dc1385fd91d4db70d
dznice <dizonnicer@gmail.com>
2023-06-08 21:39:37 +0800

    dark mode changes init

--- FRONTEND/src/app/admin/admin-home/admin-home.component.ts
+++ FRONTEND/src/app/admin/admin-home/admin-home.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
   styleUrls: ['./admin-home.component.scss'],
-  providers: [itemService]
+  providers: [itemService],
 })
 export class AdminHomeComponent implements OnInit {
   nullcheck: number = 0;
@@ -18,68 +18,66 @@ export class AdminHomeComponent implements OnInit {
   type: string;
   stat: string;
 
-  types: string[]= ["Journal Entry", "Invoice", "Payment" ];
-  stats: string[]= ["Overdue", "Pending", "Closed", "Paid" ];
+  types: string[] = ['Journal Entry', 'Invoice', 'Payment'];
+  stats: string[] = ['Overdue', 'Pending', 'Closed', 'Paid'];
 
   startDate: string = '';
-  endDate : string = '';
+  endDate: string = '';
   duedit: any;
 
   constructor(private http: HttpClient, private route: Router) {}
   ngOnInit(): void {
-      // this.showEntries();
-      this.showAccounting()
-      this.checkDue()
+    // this.showEntries();
+    this.showAccounting();
+    this.checkDue();
   }
-
 
   public entries: any;
   public account: any;
 
-last_day:any;
-today=new Date()
+  last_day: any;
+  today = new Date();
 
   showAccounting(): void {
-      this.http.get('http://127.0.0.1:8000/api/account').subscribe((res: any) => {
-        console.log(res.length);
-        this.account = res;
-          
-      });
-    }
+    this.http.get('http://127.0.0.1:8000/api/account').subscribe((res: any) => {
+      console.log(res.length);
+      this.account = res;
+    });
+  }
 
-    dueDate(): void {
-      this.http.get('http://127.0.0.1:8000/api/duedate').subscribe((res: any) => {
-        console.log(res);
-        this.duedit = res;
-      });
-    }
-
+  dueDate(): void {
+    this.http.get('http://127.0.0.1:8000/api/duedate').subscribe((res: any) => {
+      console.log(res);
+      this.duedit = res;
+    });
+  }
 
   memberInfo(data: any) {
-      this.http
-        .get('http://127.0.0.1:8000/api/memberList/' + data)
-        .subscribe((res: any) => {
-          this.route.navigateByUrl('admin/accounting/member-info/' + data);
-        });
+    this.http
+      .get('http://127.0.0.1:8000/api/memberList/' + data)
+      .subscribe((res: any) => {
+        this.route.navigateByUrl('admin/accounting/member-info/' + data);
+      });
   }
 
   memberJournal(data: any) {
-      this.http
-        .get('http://127.0.0.1:8000/api/transacList/' + data)
-        .subscribe((res: any) => {
-          if(data != null){
+    this.http
+      .get('http://127.0.0.1:8000/api/transacList/' + data)
+      .subscribe((res: any) => {
+        if (data != null) {
           console.log(data);
           this.route.navigateByUrl('admin/accounting/journal-transac/' + data);
-          }
-        });
-    }
-
-    hello:string = 'hello'
-    checkDue(){
-      console.log('checkdueee')
-      this.http.post('http://127.0.0.1:8000/api/dueDateSMS', this.hello) .subscribe((res: any) => {
-        console.log(res)
-    })
-    }
-
+        }
+      });
   }
+
+  hello: string = 'hello';
+  checkDue() {
+    console.log('checkdueee');
+    this.http
+      .post('http://127.0.0.1:8000/api/dueDateSMS', this.hello)
+      .subscribe((res: any) => {
+        console.log(res);
+      });
+  }
+}
